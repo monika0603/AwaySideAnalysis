aaaf627e5da4d25deddf4d986e7e39ea9fbc1a12
monika0603 <monika.pu@gmail.com>
2015-03-27 22:17:24 -0400

    Chasing crazy VzBins number

--- AwayAnalyzer/src/AwayAnalyzer.cc
+++ AwayAnalyzer/src/AwayAnalyzer.cc
@@ -344,8 +344,8 @@ AwayAnalyzer::initHistos(const edm::Service<TFileService> & fs)
   char histoTitle1[1000];
   char histoName2[200];
   char histoTitle2[200];
-  nVzBins = ptBins.size()-1;
-  std::cout<<ptBins.size()<<std::endl;
+  nVzBins = ptBins_.size()-1;
+  std::cout<<ptBins_.size()<<std::endl;
   std::cout<<"The number of Vz bins = "<<nVzBins<<std::endl;
     
   for(int kVz=0; kVz<nVzBins; kVz++) {
